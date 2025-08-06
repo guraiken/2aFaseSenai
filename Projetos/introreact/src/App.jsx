bcb3bac8cff4c0022765f47f529a8d4bec5983f8
guraiken <gustavo.fiabani@gmail.com>
2025-08-05 22:07:56 -0300

    introdução do react js

---
+++ Projetos/introreact/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import MotivosPraUsarReact from './components/MotivosPraUsarReact'
+import Navbar from './components/navbar'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <h1>Página de React JS</h1>
+      <MotivosPraUsarReact/>
+    </>
+  )
+}
+
+export default App
---
+++ Projetos/introreact/src/components/MotivosPraUsarReact.jsx
@@ -0,0 +1,13 @@
+import "./MotivosPraUsarReact.css"
+
+function MotivosPraUsarReact() {
+    
+    return (
+        <div className="motivos-react-container">
+        <h1>Motivos pra usar React JS!</h1>
+        <p>Um componente criado pode ser reutilizado.</p>
+        </div>
+    )
+}
+
+export default MotivosPraUsarReact
