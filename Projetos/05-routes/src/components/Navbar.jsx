e48a75120a8ef32766bc972daa1f441ef7c6ae7f
guraiken <gustavo.fiabani@gmail.com>
2025-09-30 22:13:39 -0300

    user route

--- Projetos/05-routes/src/components/Navbar.jsx
+++ Projetos/05-routes/src/components/Navbar.jsx
@@ -3,18 +3,10 @@ import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <nav>
-        <Link to={'/'}>
-        Home Page
-        </Link>
-        <Link to={'/pagina1'}>
-        Pagina Um
-        </Link>
-        <Link to={'/pagina2'}>
-        Pagina Dois
-        </Link>
-        <Link to={'/pagina3'}>
-        Pagina Tres
-        </Link>
+        <Link to={'/'}>Home Page</Link>
+        <Link to={'/pagina1'}>Pagina Um</Link>
+        <Link to={'/pagina2'}>Pagina Dois</Link>
+        <Link to={'/pagina3'}>Pagina Tres</Link>
     </nav>
   )
 }
