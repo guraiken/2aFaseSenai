2948da93c88b0d984221ff132cf58a3918e1d6e5
guraiken <gustavo.fiabani@gmail.com>
2025-10-14 22:12:38 -0300

    aula kdsakdfdjk

--- Projetos/06-crud-aula/server.js
+++ Projetos/06-crud-aula/server.js
@@ -13,7 +13,27 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
-app.post('/clientes', async)
+app.post('/clientes', async (req, res) => {
+    const {nome, endereco, email, telefone} = req.body;
+    try {
+        const [result] = await
+        connection.query(
+            `INSERT INTO
+            cliente
+            (nome, endereco, email, telefone)
+            VALUES
+            (?, ?, ?, ?)`, [nome, endereco, email, telefone]
+        );
+        const [novo_cliente] =
+        await connection.query(
+            'SELECT * FROM cliente WHERE id =  ?', [result.insertId]
+        )
+        res.status(201).json((novo_cliente[0]))
+    } catch (errinho) {
+        console.error(errinho.message)
+        res.status(500).json({erro: "Falha do servidor ao criar o cliente"})
+    }
+})
 
 app.get('/', (req, res)=> {
     res.send("Hello world!")
@@ -21,5 +41,5 @@ app.get('/', (req, res)=> {
 
 
 app.listen(port, ()=> {
-    console.log("API funcionando")
+    console.log("API funcionando 2.0")
 })
